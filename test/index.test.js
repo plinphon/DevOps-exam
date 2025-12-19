ebdb093548de0f8d85069f9b4e83af16b4331930
plinphon <greatez12345@gmail.com>
2025-12-19 09:35:15 +0700

    fixed path

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 const test = require('node:test');
 const assert = require('assert/strict');
-const { Sample, server } = require('./index');
+const { Sample, server } = require('../index');
 test('Sample test',
      () => assert.equal(Sample("localhost").url,
                        "localhost"));
